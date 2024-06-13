c8f82a9947b277a78e1ca1b6138bb5a844269ea8
Devlin McClure <devlincodes@pm.me>
2024-06-13 10:05:07 -0400

    Add Auto Updater

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require("electron");
 const remoteMain = require("@electron/remote/main");
-var path = require("path");
+const { autoUpdater } = require("electron-updater");
 
 remoteMain.initialize();
 
@@ -16,6 +16,7 @@ app.on("window-all-closed", function () {
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
 app.on("ready", function () {
+	autoUpdater.checkForUpdatesAndNotify();
 	// Create the browser window.
 	mainWindow = new BrowserWindow({
 		width: 1000,
